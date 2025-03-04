4b6dacacd67f233544c731e65eb2a0b588e443ac
Arthur Uenoyama Dezem <uenoarthur@outlook.com>
2025-03-03 22:56:19 -0300

    Update hotel.sql

--- Hotel/hotel.sql
+++ Hotel/hotel.sql
@@ -16,6 +16,7 @@ CREATE TABLE funcionarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL,
     cargo VARCHAR(50) NOT NULL,
     telefone VARCHAR(20) NOT NULL
 );
